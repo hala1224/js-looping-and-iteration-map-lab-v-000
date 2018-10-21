c3233ab63f5975251de2ad8e7b312524b0dc302d
Natalie Halawani <hala1224@gmail.com>
2018-10-21 17:46:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -33,22 +33,6 @@ function attributesToPhrase(obj) {
         console.log('')
       }
   
-   it('converts to list saying the name and where each individual is from', () => {
-      const drivers = [
-        { name: 'Bobby', hometown: 'Pittsburgh' },
-        { name: 'Sammy', hometown: 'New York' },
-        { name: 'Sally', hometown: 'Cleveland' },
-        { name: 'Annette', hometown: 'Los Angeles' },
-        { name: 'Bobby', hometown: 'Tampa Bay' }
-      ];
-
-      expect(attributesToPhrase(drivers)).to.eql([
-        'Bobby is from Pittsburgh',
-        'Sammy is from New York',
-        'Sally is from Cleveland',
-        'Annette is from Los Angeles',
-        'Bobby is from Tampa Bay'
-      ]);
-  
+   
   
 }
