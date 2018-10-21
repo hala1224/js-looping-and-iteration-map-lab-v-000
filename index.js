b6ca7bc7a5fe269eabf0f7a0a1d842105634f9e3
Natalie Halawani <hala1224@gmail.com>
2018-10-21 16:56:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file.
+
+function lowerCaseDrivers(arr) {
+  
+  const lowerCase = arr.map(x => x.toLowerCase());
+
+  return lowerCase;
+  
+}
+
+function nameToAttributes(arr) {
+   for ( const element of array) {
+     
+   }
+  
+  
+  
+}
+
+
+
+function attributesToPhrase(obj) {
+  
+  
+}
